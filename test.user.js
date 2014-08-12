d330e3f5c06916665b8b2d9c8b22cfc597757040
Darek Kay <darekkay@gmail.com>
2014-08-12 23:30:30 +0200

    Userscript update test

---
+++ test.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @version         1.0
+// @downloadURL     https://github.com/darekkay/userscripts/raw/master/test.user.js
+// @include         https://github.com
+// ==/UserScript==
+
+console.log("Hello world");
